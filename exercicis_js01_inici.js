5a8731008144c8f434566b10a92980a158d67e09
u292 <u292kl@gmail.com>
2020-03-19 22:38:10 +0100

    arrow functions convert

--- exercicis_js01_inici.js
+++ exercicis_js01_inici.js
@@ -1,12 +1,13 @@
-// numeros(1, 2)
-
-function numeros(a, b) {
+const numeros = function(a, b) {
     console.log(a, b)
 }
 
-// calcula(2, 3)
+const numerosArr = (a, b) => console.log(a, b)
 
-function calcula(a, b) {
+// numeros(1, 2)
+// numerosArr(1, 2)
+
+const calcula = function(a, b) {
     let s = a + b
     let r = a - b
     let p = a * b
@@ -14,18 +15,26 @@ function calcula(a, b) {
     console.log("Resultados:\n" + a + " + " + b + " = " + s + "\n" + a + " - " + b + " = " + r + "\n" + a + " * " + b + " = " + p + "\n" + a + " / " + b + " = " + d)
 }
 
-// xx(40, 60)
+const calculaArr = (a, b) => console.log(`Resultados:
+${a} + ${b} = ${a+b}
+${a} - ${b} = ${a-b}
+${a} * ${b} = ${a*b}
+${a} / ${b} = ${a/b}`)
 
-function xx(chicos, chicas) {
+// calcula(2, 3)
+// calculaArr(2, 3)
+
+const percent = function(chicas, chicos) {
     percentGirls = chicas / (chicos + chicas) * 100
     console.log("El porcentaje de chicas es " + percentGirls + "%")
 }
 
-// compara(2, 3)
-// compara(4, 3)
-// compara(3, 3)
+const percentArr = (a, b) => console.log(`El porcentaje de chicas es ${a/(a+b)*100}%`)
 
-function compara(a, b) {
+// percent(60, 40)
+// percentArr(60, 40)
+
+const compara = function(a, b) {
     if (a > b) {
         console.log("El numero mayor es " + a)
     } else if (a < b) {
@@ -35,11 +44,17 @@ function compara(a, b) {
     }
 }
 
-// mayor(1, 2, 3)
-// mayor(1, 3, 2)
-// mayor(3, 1, 2)
+const comparaArr = (a, b) => (a > b) ? console.log(`El numero mayor es ${a}`) : ((b > a) ? console.log(`El numero mayor es ${b}`) : console.log(`Ambos numeros son iguales`))
 
-function mayor(a, b, c) {
+// compara(2, 3)
+// compara(4, 3)
+// compara(3, 3)
+// comparaArr(2, 3)
+// comparaArr(4, 3)
+// comparaArr(3, 3)
+
+
+const mayor = function(a, b, c) {
     if (a > b && a > c) {
         console.log("El numero mayor es " + a)
     } else if (b > a && b > c) {
@@ -49,11 +64,16 @@ function mayor(a, b, c) {
     }
 }
 
-// operacion(3, 4, "s")
-// operacion(5, 2, "m")
-// operacion(5, 2, "masd")
+const mayorArr = (a, b, c) => (a > b && a > c) ? console.log(`El numero mayor es ${a}`) : ((b > a && b > c) ? console.log(`"El numero mayor es ${b}`) : console.log(`El numero mayor es ${c}`))
 
-function operacion(a, b, op) {
+// mayor(1, 2, 3)
+// mayor(1, 4, 2)
+// mayor(5, 1, 2)
+// mayorArr(1, 2, 3)
+// mayorArr(1, 4, 2)
+// mayorArr(5, 1, 2)
+
+const operacion = function(a, b, op) {
     if (op == "s") {
         console.log("La suma es " + (a + b))
     } else if (op == "m") {
@@ -63,10 +83,16 @@ function operacion(a, b, op) {
     }
 }
 
-// anyo(2020)
-// anyo(2021)
+const operacionArr = (a, b, op) => (op == "s") ? console.log(`La suma es ${a+b}`) : ((op == "m") ? console.log(`El producto es ${a*b}`) : console.log(`Operacion desconocida`))
 
-function anyo(year) {
+// operacion(3, 4, "s")
+// operacion(5, 2, "m")
+// operacion(5, 2, "masd")
+// operacionArr(3, 4, "s")
+// operacionArr(5, 2, "m")
+// operacionArr(5, 2, "masd")
+
+const anyo = function(year) {
     if ((year % 4) == 0 && (year % 100) != 0 || (year % 400) == 0) {
         console.log("El año " + year + " es bisiesto.")
     } else {
@@ -74,24 +100,38 @@ function anyo(year) {
     }
 }
 
-//getNumber_1_10()
+const anyoArr = year => ((year % 4) == 0 && (year % 100) != 0 || (year % 400) == 0) ? console.log(`El año ${year} es bisiesto.`) : console.log(`El año ${year} NO es bisiesto.`)
 
-function getNumber_1_10() {
-    let num = parseInt(prompt("Entre un numero entre 1 y 10 ==>"))
+// anyo(2020)
+// anyo(2021)
+// anyoArr(2020)
+// anyoArr(2021)
+
+const getNumber_1_10 = function() {
+    let num = getNumber("Entre un numero entre 1 y 10")
     while ((num > 10) || (num < 1)) {
-        num = parseInt(prompt("Numero incorrecto, entrar de nuevo ==>"))
+        num = getNumber("Numero incorrecto, entrar de nuevo")
     }
     console.log("El cuadrado de " + num + " es " + (num * num))
 }
 
-function getNumber(msg) {
-    return parseInt(prompt(msg + " ==>"))
+const getNumber = function(msg) {
+    return +(prompt(msg + " ==>"))
 }
 
+const getNumberArr = msg => +prompt(`${msg} =>`)
 
-// sumNotNegative()
+const getNumber_1_10Arr = () => {
+    let num = null
+    do {
+        (num == null) ? (num = +prompt("Entre un numero entre 1 y 10")) : (num = +prompt("Numero incorrecto, entrar de nuevo"))
+    } while ((num > 10) || (num < 1))
+}
 
-function sumNotNegative(sum = 0) {
+//getNumber_1_10()
+// getNumber_1_10Arr()
+
+const sumNotNegative = function(sum = 0) {
     let num = getNumber("Numero a sumar (<=0 para resultado)")
     if (num > 0) {
         sum += num
@@ -101,25 +141,46 @@ function sumNotNegative(sum = 0) {
     }
 }
 
-function arrayMin(numArray) {
+const sumNotNegativeArr = (sum = 0, num = +prompt("Numero a sumar (<=0 para resultado)")) => (num > 0) ? sumNotNegative(sum += num) : console.log(`La suma de los valores introducidos es ${sum}`)
+
+// sumNotNegative()
+//sumNotNegativeArr()
+
+const arrayMin = function(numArray) {
     return numArray.sort(function(a, b) { return a - b })[0]
 }
 
-function arrayMax(numArray) {
+const arrayMinArr = numArray => numArray.sort((a, b) => a - b)[0]
+
+// console.log(arrayMinArr([2, 3, 4, 1, 3, 5, 2]))
+
+const arrayMax = function(numArray) {
     return numArray.sort(function(a, b) { return a - b })[numArray.length - 1]
 }
 
-function arraySum(numArray) {
+const arrayMaxArr = numArray => numArray.sort((a, b) => a - b)[numArray.length - 1]
+
+// console.log(arrayMaxArr([2, 3, 4, 1, 3, 5, 2]))
+
+const arraySum = function(numArray) {
     return numArray.reduce((a, b) => a + b, 0)
 }
 
-function arrayAverage(numArray) {
+const arraySumArr = numArray => numArray.reduce((a, b) => a + b, 0)
+
+// console.log(arraySumArr([2, 3, 4, 1, 3, 5, 2]))
+
+const arrayAverage = function(numArray) {
     return arraySum(numArray) / numArray.length
 }
 
+const arrayAverageArr = numArray => (arraySum(numArray) / numArray.length)
+
+// console.log(arrayAverageArr([2, 3, 4, 1, 3, 5, 2]))
+
 // sumStatsNotNegative([])
 
-function sumStatsNotNegative(numArray = []) {
+const sumStatsNotNegative = function(numArray = []) {
     let num = getNumber("Numeros (<=0 para resultado)")
     if (num > 0) {
         numArray[numArray.length] = (num)
@@ -131,9 +192,9 @@ function sumStatsNotNegative(numArray = []) {
     }
 }
 
-// calcFactorial(5)
+// showFactorial(5)
 
-function factorial(num) {
+const factorial = function(num) {
     if (num <= 1) {
         return 1
     } else {
@@ -141,13 +202,13 @@ function factorial(num) {
     }
 }
 
-function calcFactorial(num) {
+const showFactorial = function(num) {
     console.log("El producto factorial de " + num + " es " + factorial(num))
 }
 
 // sumEven(10)
 
-function sumEven(num) {
+const sumEven = function(num) {
     let sum = 0
     for (i = 1; i <= num; i++) {
         if ((i % 2) == 0) {
@@ -159,7 +220,7 @@ function sumEven(num) {
 
 // inputPassword()
 
-function checkPassword(pass) {
+const checkPassword = function(pass) {
     if (pass == "rebeca") {
         return true
     } else {
@@ -167,7 +228,7 @@ function checkPassword(pass) {
     }
 }
 
-function attemptPassword(pass, attempt = 1) {
+const attemptPassword = function(pass, attempt = 1) {
     if (attempt < 3) {
         if (checkPassword(pass)) {
             console.log("Contraseña correcta")
@@ -181,18 +242,19 @@ function attemptPassword(pass, attempt = 1) {
     }
 }
 
-function inputPassword() {
+const inputPassword = function() {
     attemptPassword(prompt("Contraseña ==> "))
 }
 
-// bombDelay(5)
 
-function bombDelay(time) {
+
+const bombDelay = function(time) {
     if (time > 0) {
         console.log(time + "...")
         setTimeout(bombDelay, 1000, time - 1)
     } else {
         console.log("BOOOOOOOOM!")
     }
-
 }
+
+// bombDelay(5)
